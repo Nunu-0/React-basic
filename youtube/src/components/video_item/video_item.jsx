1334b49969d3e17bbcf5fd894fdd898fcd3a57cf
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2022-05-03 20:44:00 +0900

    make video item

--- youtube/src/components/video_item/video_item.jsx
+++ youtube/src/components/video_item/video_item.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import styles from './video_item.module.css'
 
-const VideoItem = props => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = ({video: {snippet : s}})=>(
+    <li className={styles.container}>
+        <div className={styles.video}>
+        <img className={styles.thumbnail}
+        src={s.thumbnails.medium.url} 
+        alt="video thumbnail" />
+        <div className={styles.metaData}>
+            <p className={styles.title}>{s.title}</p>
+            <p className={styles.channel}>{s.channelTitle}</p>
+        </div>
+        </div>
+    </li>
+);
 
 export default VideoItem;
