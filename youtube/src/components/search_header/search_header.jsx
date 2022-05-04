b992823087f041e9a9130b00addb11428b251022
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2022-05-04 19:32:16 +0900

    youtube detail

--- youtube/src/components/search_header/search_header.jsx
+++ youtube/src/components/search_header/search_header.jsx
@@ -1,7 +1,7 @@
 import styles from './search_header.module.css';
 import React, { useRef } from 'react';
 
-const Search_header = ({onSearch}) => {
+const SearchHeader = ({onSearch}) => {
     const inputRef = useRef();
     const handleSearch = () =>{
         const value = inputRef.current.value;
@@ -18,15 +18,19 @@ const Search_header = ({onSearch}) => {
     return(
         <header className={styles.header}>
             <div className={styles.logo}>
-                <img className={styles.img} src="/images/logo.png" alt="logo" />
+                <img className={styles.img} 
+                src="/images/logo.png" alt="logo" />
                 <h1 className={styles.title}>Youtube</h1>
             </div>
-            <input ref={inputRef} className={styles.input} type="search" placeholder='Search...' onKeyPress={onKeyPress}/>
+            <input ref={inputRef} className={styles.input} 
+            type="search" placeholder='Search...' 
+            onKeyPress={onKeyPress}/>
             <button className={styles.button} type='submit' onClick={onClick}>
-                <img className={styles.buttonImg} src="/images/search.png" alt="search" />
+                <img className={styles.buttonImg} 
+                src="/images/search.png" alt="search" />
             </button>
         </header>
-    )
+    );
 };
 
-export default Search_header;
+export default SearchHeader;
